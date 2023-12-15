3c75df8c1370368c11ca52007f77622d74e9abad
sunil <palegar45@gmail.com>
2023-12-15 16:30:48 +0530

    Add project files.

---
+++ alt letters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alt_letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alternate letters from a to z:");
+
+            for (char letter = 'a'; letter <= 'z';letter = (char) (letter + 2))
+            {
+                Console.Write($"{letter} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
